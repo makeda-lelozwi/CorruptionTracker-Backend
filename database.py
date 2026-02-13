313db5b49f033c61678e5f95cdfb9de517d97e72
Lelo Dumeko <Lelozwi@nawirigroup.com>
2026-02-13 16:15:49 +0200

    Move credentials to .env

--- database.py
+++ database.py
@@ -1,8 +1,19 @@
+import os
+from dotenv import load_dotenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE="postgresql://lelodumeko:dev1234!@localhost:5432/corruption_tracker_dev"
+load_dotenv()
+
+URL_DATABASE = os.getenv("URL_DATABASE")
+if not URL_DATABASE:
+    raise RuntimeError("URL_DATABASE environment variable is not set")
+
+# Render uses "postgres://" but SQLAlchemy requires "postgresql://"
+if URL_DATABASE.startswith("postgres://"):
+    URL_DATABASE = URL_DATABASE.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(URL_DATABASE)
 
